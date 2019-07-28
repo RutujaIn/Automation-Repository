7b3f610afe80e1fde7112c2920253e242dc6cf39
Rutuja <rutuja.inamdar@gmail.com>
2019-07-28 13:36:04 +0530

    Commit of second java class

---
+++ BasicCoreJava/src/variablesAndDatatypes/SecondJavaClass.java
@@ -0,0 +1,24 @@
+package variablesAndDatatypes;
+
+public class SecondJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Arithmetic operations
+		
+		int a = 10;
+		int b = 20;
+		// +,-,*,/,%(reminder/mode)
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+
+
+		
+
+	}
+
+}
